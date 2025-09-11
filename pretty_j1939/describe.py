050060a38745673ced5188d1463013acb8c8873f
LP <lpafortier@gmail.com>
2025-09-11 09:12:40 -0400

    Changed the location of the [Unit] display.

--- pretty_j1939/describe.py
+++ pretty_j1939/describe.py
@@ -227,8 +227,8 @@ class DADescriber:
         for spn in pgn_object.get("SPNs", []):
             if skip_spns.get(spn, ()) != ():  # skip any SPNs that have already been processed.
                 continue
-            spn_name = "(" + str(spn) + ")" + self.get_spn_name(spn)
-            spn_units = self.spn_objects.get(spn)["Units"]
+            spn_name = "(" + str(spn) + ")" + self.get_spn_name(spn) + "[" + self.spn_objects.get(spn)["Units"] + "]"
+            spn_units = "" # self.spn_objects.get(spn)["Units"]
 
             def mark_spn_covered(new_spn, new_spn_name, new_spn_description):
                 skip_spns[new_spn] = (new_spn_name, new_spn_description)  # TODO: move this closer to real-time handling
